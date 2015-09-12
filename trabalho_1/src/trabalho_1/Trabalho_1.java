837214cf7dddc589833cb751fe51690217872587
Maicon <Maicon@172.16.101.3>
2015-09-11 21:32:53 -0300

    Iniciando, testando, excluíndo alguma coisa...

--- trabalho_1/src/trabalho_1/Trabalho_1.java
+++ trabalho_1/src/trabalho_1/Trabalho_1.java
@@ -3,11 +3,8 @@ package trabalho_1;
 
 public class Trabalho_1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO a bagaça toda
     }
     
 }
