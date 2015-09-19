874f54c39f6ab78c7bedc6552329089c070df4bb
comp1 <comp1@comp1_24.lab.unesc.net>
2015-09-18 21:38:30 -0300

    Ajuste nas ações dos botões ok

--- trabalho_1/src/trabalho_1/Trabalho_1.java
+++ trabalho_1/src/trabalho_1/Trabalho_1.java
@@ -3,7 +3,6 @@ package trabalho_1;
 public class Trabalho_1 {
 
     public static void main(String[] args) {
-        Botao botoes = new Botao();
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new LoginJFrame().setVisible(true);
